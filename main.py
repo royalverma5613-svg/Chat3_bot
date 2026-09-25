8008ba5c57317b65d2292367d1e7ed1875330909
royalverma5613-svg <royal.verma5613@gmail.com>
2026-09-25 15:11:48 +0530

    main.py

--- main.py
+++ main.py
@@ -10,7 +10,7 @@ from datetime import datetime
 from flask import Flask
 
 # === BOT CONFIGURATION ===
-BOT_TOKEN = "YAHAN_APNA_NAYA_TOKEN_PASTE_KAREIN"
+BOT_TOKEN = "8926312414:AAFAnEc5NizNvBnAWKyn1s1CjuRtgZz37bQ"
 CHANNEL_USERNAME = "@ai2kmm"
 ADMIN_USERNAME = "@egofiremax"
 ADMIN_GROUP = "-1004322519230"  # Aapki Admin Group ID
